c3fff998f526618a48d5b2f9d7053289a78b2678
Steve Brown <sbrown@dakarsoftware.com>
2021-07-16 08:56:47 +0200

    Rollup before demo

--- table_dump/tb_dump.sql
+++ table_dump/tb_dump.sql
@@ -1802,4 +1802,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-15 16:40:42
+-- Dump completed on 2021-07-16  8:54:44
